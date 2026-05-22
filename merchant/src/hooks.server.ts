c9113c50c45ba7a705d4496027dae36e1df80ce5
sabarisrinivasan <sabarivasan522@gmail.com>
2026-05-22 11:50:55 +0530

    added origin checker

--- merchant/src/hooks.server.ts
+++ merchant/src/hooks.server.ts
@@ -4,7 +4,9 @@ export const handle: Handle = async ({ event, resolve }) => {
   // Fix: leading slash added
   if (event.url.pathname === '/api/payment/callback') {
     const originalRequest = event.request;
-    
+    const origin  = event.request.headers.get('origin');
+    const referer = event.request.headers.get('referer');
+    console.log(origin,referer)
     // Create new request with spoofed origin to bypass CSRF
     event.request = new Request(originalRequest, {
       headers: (() => {
